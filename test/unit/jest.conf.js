7269a70b90420fe8b1d907d615b417d4ecefe841
EvodiaAut <evodia.aut@gmail.com>
2018-02-08 21:35:46 +0100

    template update

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -14,15 +14,12 @@ module.exports = {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
-  testPathIgnorePatterns: [
-    '<rootDir>/test/e2e'
-  ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
-    'src/**/*.{js,vue}',
+    'src/components/**/*.{js,vue}',
     '!src/main.js',
     '!**/node_modules/**'
   ]
